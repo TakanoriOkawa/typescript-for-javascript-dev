a8fda1163df0e41c4b1910829f0b429922b18f82
test <okawatakanori9916@gmail.com>
2021-01-23 22:41:15 +0900

    fix ts-node-dev

---
+++ src/boolean.ts
@@ -0,0 +1,9 @@
+//モジュール化している jsのコードがエラーになる
+export {};
+
+// let name = 'TypeScript';
+
+let isF: boolean = true;
+isF = false;
+isF = 1;
+console.log({ isF });
--- src/install-typescript.ts
+++ src/install-typescript.ts
@@ -1,2 +1,3 @@
+//プリミティブ型
 let message: string = 'Hello TypeScript!!!!';
 console.log({ message });
